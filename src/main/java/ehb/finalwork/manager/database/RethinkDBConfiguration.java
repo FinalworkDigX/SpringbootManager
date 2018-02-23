18f49c4f3ea9dc7e6e568db9fd7d2459b1dea598
Unknown <ludovic.marchand@hotmail.be>
2018-02-23 01:20:00 +0100

    wip

--- src/main/java/ehb/finalwork/manager/database/RethinkDBConfiguration.java
+++ src/main/java/ehb/finalwork/manager/database/RethinkDBConfiguration.java
@@ -1,16 +1,24 @@
 package ehb.finalwork.manager.database;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.Resource;
 
 @Configuration
 @PropertySource("classpath:rethink.properties")
 @ConfigurationProperties(prefix = "rethink")
 public class RethinkDBConfiguration {
 //    public static final String DBHOST = "10.3.50.6";
-    private static final String DBHOST = "192.168.0.115";
+//    private static final String DBHOST = "192.168.0.115";
+//    private static final String DBHOST = "fw.ludovicmarchand.be";
+    private static final String DBHOST = "94.224.39.64";
+
+    @Value(value = "classpath:certfile.crt")
+    private Resource certfile;
+
     private String username;
     private String password;
 
@@ -32,7 +40,7 @@ public class RethinkDBConfiguration {
 
     @Bean
     public RethinkDBConnectionFactory connectionFactory() {
-        return new RethinkDBConnectionFactory(DBHOST, username, password);
+        return new RethinkDBConnectionFactory(DBHOST, username, password, certfile);
     }
 
     @Bean
--- src/main/java/ehb/finalwork/manager/database/RethinkDBConnectionFactory.java
+++ src/main/java/ehb/finalwork/manager/database/RethinkDBConnectionFactory.java
@@ -2,24 +2,51 @@ package ehb.finalwork.manager.database;
 
 import com.rethinkdb.RethinkDB;
 import com.rethinkdb.net.Connection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
+import javax.servlet.ServletContext;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 public class RethinkDBConnectionFactory {
+
+
+    private static final RethinkDB r = RethinkDB.r;
+    private final Logger log = LoggerFactory.getLogger(DBInitializer.class);
 
     private String host;
     private String username;
     private String password;
+    private Resource certfile;
 
-    public RethinkDBConnectionFactory(String host, String username, String password) {
+    public RethinkDBConnectionFactory(String host, String username, String password, Resource certfile) {
         this.host = host;
         this.username = username;
         this.password = password;
+        this.certfile = certfile;
     }
 
     public Connection createConnection() {
         //Automatically closes after 20seconds
-        return RethinkDB.r.connection()
-                .hostname(this.host)
-                .user(username, password)
-                .connect();
+
+        try {
+            InputStream cert = certfile.getInputStream();
+
+            return RethinkDB.r.connection()
+                    .hostname(this.host)
+                    .user(username, password)
+                    .port(2096)
+                    .certFile(cert)
+                    .connect();
+        }
+        catch (Exception e) {
+            log.warn("fuck");
+            log.warn(e.getMessage());
+        }
+        return null;
     }
 }
