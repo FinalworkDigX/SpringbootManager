56f8e22a49dd7503cd5b6c40cd5ac629863ae140
Unknown <ludovic.marchand@hotmail.be>
2018-03-09 20:23:52 +0100

    DataLogControllerTest
    added:
      => getAll
      => create

--- src/test/java/ehb/finalwork/manager/controller/DataLogControllerTest.java
+++ src/test/java/ehb/finalwork/manager/controller/DataLogControllerTest.java
@@ -1,107 +1,119 @@
 package ehb.finalwork.manager.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import ehb.finalwork.manager.TestUtil;
 import ehb.finalwork.manager.dto.RethinkDataLogDto;
 import ehb.finalwork.manager.model.DataLog;
 import ehb.finalwork.manager.model.Information;
+import ehb.finalwork.manager.service.DataLogService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import javax.sound.sampled.Line;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import static java.lang.Math.toIntExact;
 
+import static java.lang.Math.toIntExact;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.hamcrest.core.Is.is;
-import static org.mockito.BDDMockito.given;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(DataLogController.class)
+@WebMvcTest(SpringJUnit4ClassRunner.class)
 public class DataLogControllerTest {
 
     private List<Information> infoList;
     private Long timestamp;
+    private DataLog dataLog;
+    private RethinkDataLogDto dataLogDto;
 
-    @Autowired
+    // Mock
     private MockMvc mockMvc;
 
-    @MockBean
+    @Mock
+    private DataLogService dataLogService;
+
+    @InjectMocks
     private DataLogController dataLogController;
 
     @Before
-    public void setup() {
+    public void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(dataLogController).build();
+
         infoList = new ArrayList<Information>();
         infoList.add(new Information("name_1", "data_1", 1L));
         infoList.add(new Information("name_2", "data_2", 2L));
 
         timestamp = Instant.now().getEpochSecond();
+        dataLog = new DataLog("id_1", "item_id_1", infoList, timestamp);
+        dataLogDto = new RethinkDataLogDto("item_id_1", infoList, timestamp);
     }
 
     @Test
     public void getDataLogsTest() throws Exception {
 
-        DataLog dl1 = new DataLog("111", "item_id_1", infoList, timestamp);
-        DataLog dl2 = new DataLog("222", "item_id_2", infoList, timestamp);
+        List<DataLog> allDataLogs = Collections.singletonList(dataLog);
 
-        List<DataLog> allDataLogs = new ArrayList<DataLog>();
-        allDataLogs.add(dl1);
-        allDataLogs.add(dl2);
-
-        when(dataLogController.getDataLogs()).thenReturn(allDataLogs);
+        when(dataLogService.getAll()).thenReturn(allDataLogs);
 
         mockMvc.perform(get("/v1/dataLog"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
-                .andExpect(jsonPath("$", hasSize(2)))
+               .andExpect(status().isOk())
+               .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
 
-                .andExpect(jsonPath("$[0].id", is("111")))
-                .andExpect(jsonPath("$[0].item_id", is("item_id_1")))
-                .andExpect(jsonPath("$[0].information", hasSize(2)))
-                .andExpect(jsonPath("$[0].timestamp", is(toIntExact(timestamp))));
+               .andExpect(jsonPath("$", hasSize(1)))
+               .andExpect(jsonPath("$[0].id", is("id_1")))
+               .andExpect(jsonPath("$[0].item_id", is("item_id_1")))
+               .andExpect(jsonPath("$[0].information", hasSize(2)))
+               .andExpect(jsonPath("$[0].timestamp", is(toIntExact(timestamp))));
 
-        verify(dataLogController, times(1)).getDataLogs();
-        verifyNoMoreInteractions(dataLogController);
+        verify(dataLogService, times(1)).getAll();
+        verifyNoMoreInteractions(dataLogService);
     }
 
     @Test
     public void addDataLogTest() throws Exception {
 
-        RethinkDataLogDto create_val = new RethinkDataLogDto("test_1", infoList, timestamp);
-        DataLog return_val = new DataLog("id", "test_1", infoList, timestamp);
+        when(dataLogService.create(any(RethinkDataLogDto.class))).thenReturn(dataLog);
 
-        given(dataLogController.createDataLog(create_val)).willReturn(return_val);
-
-        mockMvc.perform(
-                post("/v1/dataLog")
-                        .content(asJsonString(create_val))
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON)
+        mockMvc.perform(post("/v1/dataLog")
+                            .content(TestUtil.convertObjectToJsonBytes(dataLogDto))
+                            .contentType(MediaType.APPLICATION_JSON)
         )
-                .andExpect(status().isOk());
-//                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE));
-//                .andExpect(jsonPath("$", hasSize(1)));
-    }
+               .andExpect(status().isOk())
+               .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
 
-    public static String asJsonString(final Object obj) {
-        try {
-            return new ObjectMapper().writeValueAsString(obj);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+               .andExpect(jsonPath("$.id", is("id_1")))
+               .andExpect(jsonPath("$.item_id", is("item_id_1")))
+               .andExpect(jsonPath("$.information", hasSize(2)))
+               .andExpect(jsonPath("$.information[0].name", is("name_1")))
+               .andExpect(jsonPath("$.information[0].data", is("data_1")))
+               .andExpect(jsonPath("$.information[0].index", is(1)))
+               .andExpect(jsonPath("$.timestamp", is(toIntExact(timestamp))));
+
+        ArgumentCaptor<RethinkDataLogDto> dtoCaptor = ArgumentCaptor.forClass(RethinkDataLogDto.class);
+        verify(dataLogService, times(1)).create(dtoCaptor.capture());
+        verifyNoMoreInteractions(dataLogService);
+
+        RethinkDataLogDto dtoArgument = dtoCaptor.getValue();
+        assertThat(dtoArgument.getItem_id(), is("item_id_1"));
+        assertThat(dtoArgument.getTimestamp(), is(timestamp));
+        assertThat(dtoArgument.getInformation(), hasSize(2));
+        assertThat(dtoArgument.getInformation().get(0).getIndex(), is(1L));
+        assertThat(dtoArgument.getInformation().get(0).getData(), is("data_1"));
+        assertThat(dtoArgument.getInformation().get(0).getName(), is("name_1"));
     }
 }
