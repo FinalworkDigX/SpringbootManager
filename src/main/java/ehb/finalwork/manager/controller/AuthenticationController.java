beaa4bd58fdc6d3862a043b4a37379fcf421badc
Unknown <ludovic.marchand@hotmail.be>
2018-03-16 00:55:25 +0100

    Auth0 module update + auth route changed

--- src/main/java/ehb/finalwork/manager/controller/AuthenticationController.java
+++ src/main/java/ehb/finalwork/manager/controller/AuthenticationController.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/v1/auth")
 public class AuthenticationController {
 
     private Logger log = LoggerFactory.getLogger(getClass());
