d741070d9f247fc1fd99d466a9777b7ec54a1855
Unknown <ludovic.marchand@hotmail.be>
2018-03-27 15:35:54 +0200

    WIP RoomController return RoomForAR

--- src/main/java/ehb/finalwork/manager/controller/RoomController.java
+++ src/main/java/ehb/finalwork/manager/controller/RoomController.java
@@ -1,11 +1,17 @@
 package ehb.finalwork.manager.controller;
 
 import ehb.finalwork.manager.dto.RethinkRoomDto;
+import ehb.finalwork.manager.dto.RoomForARDto;
+import ehb.finalwork.manager.model.Beacon;
 import ehb.finalwork.manager.model.Room;
+import ehb.finalwork.manager.service.DataItemService;
 import ehb.finalwork.manager.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,6 +22,9 @@ public class RoomController {
 
     @Autowired
     private RoomService roomService;
+
+    @Autowired
+    private DataItemService dataItemService;
 
     @GetMapping()
     public List<Room> getRooms() {
@@ -35,5 +44,12 @@ public class RoomController {
     @DeleteMapping("/{rid}")
     public void deleteRoom(@PathVariable String rid) {
         roomService.delete(rid);
+    }
+
+    @MessageMapping("/room/{privateChannel}/{roomId}")
+    @SendTo("/topic/room/{privateChannel}")
+    public RoomForARDto roomForAR(@DestinationVariable String privateChannel, @DestinationVariable String roomId, Beacon beacon) {
+
+        return new RoomForARDto();
     }
 }
